9e48463504ea808695347de040d00a998b62fc58
Francois Campbell <campbell.francois@gmail.com>
2016-01-10 17:54:57 -0500

    Rudimentary time and date picker

--- mobile/src/main/kotlin/io/github/francoiscampbell/manytimes/MainActivity.kt
+++ mobile/src/main/kotlin/io/github/francoiscampbell/manytimes/MainActivity.kt
@@ -1,23 +1,40 @@
 package io.github.francoiscampbell.manytimes
 
+import android.app.DatePickerDialog
+import android.app.TimePickerDialog
 import android.os.Bundle
 import android.support.design.widget.FloatingActionButton
 import android.support.design.widget.Snackbar
+import android.support.v7.app.AlertDialog
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.Toolbar
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Toast
+import butterknife.bindView
 
 class MainActivity : AppCompatActivity() {
+    private val toolbar: Toolbar by bindView(R.id.toolbar)
+    private val fab: FloatingActionButton by bindView(R.id.fab)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val toolbar = findViewById(R.id.toolbar) as Toolbar
         setSupportActionBar(toolbar)
 
-        val fab = findViewById(R.id.fab) as FloatingActionButton
-        fab.setOnClickListener { view -> Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG).setAction("Action", null).show() }
+        fab.setOnClickListener {
+            Snackbar.make(it, "New timer", Snackbar.LENGTH_LONG).show()
+            val timePickerDialog = TimePickerDialog(this, { timePicker, hour, minute ->
+                Toast.makeText(this, "Time set: $hour:$minute", Toast.LENGTH_SHORT).show()
+            }, 0, 0, false)
+            timePickerDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "Pick Date", { dialogInterface, which ->
+                val datePickerDialog = DatePickerDialog(this, DatePickerDialog.OnDateSetListener { datePicker, year, month, day ->
+                    Toast.makeText(this, "Date set: $year-$month-$day ", Toast.LENGTH_SHORT).show()
+                }, 2016, 1, 1)
+                datePickerDialog.show()
+            })
+            timePickerDialog.show()
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
